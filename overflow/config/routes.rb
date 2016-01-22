021103611db9da9f99550ceb6b33579bb390fc87
Natalie Polen <email": null>
2016-01-22 17:20:48 -0500

    Create questions#index route and view

--- overflow/config/routes.rb
+++ overflow/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  root 'questions#index'
 
   resources :questions do
     resources :comments
@@ -17,7 +18,6 @@ Rails.application.routes.draw do
   end
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
