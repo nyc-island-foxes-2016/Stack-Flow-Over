699526fe20375bcc447ea87c521d1c7550ed6c95
Natalie Polen <email": null>
2016-01-22 16:48:37 -0500

    Seed comments and votes

--- overflow/db/seeds.rb
+++ overflow/db/seeds.rb
@@ -24,7 +24,21 @@ end
   Comment.create(body: Faker::Lorem.sentence, user_id: User.all.sample.id, commentable_id: Answer.all.sample.id, commentable_type: "Answer" )
 end
 
+vote_value = [1, -1]
+4.times do
 
+  rand(1..5).times do
+    Vote.create(user_id: User.all.sample.id, value: vote_value.sample, voteable_id:Comment.all.sample.id, voteable_type: "Comment" )
+  end
+
+  rand(1..5).times do
+    Vote.create(user_id: User.all.sample.id, value: vote_value.sample, voteable_id:Question.all.sample.id, voteable_type: "Question")
+  end
+
+  rand(1..5).times do
+    Vote.create(user_id: User.all.sample.id, value: vote_value.sample, voteable_id:Answer.all.sample.id, voteable_type: "Answer")
+  end
+end
 
 
 
