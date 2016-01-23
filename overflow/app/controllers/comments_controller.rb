ebd157aff34db6fe40a37b58dcfe50347c999bf0
Chris Guard <cguard90@gmail.com>
2016-01-23 13:32:52 -0500

    amend routing to save and display comment

--- overflow/app/controllers/comments_controller.rb
+++ overflow/app/controllers/comments_controller.rb
@@ -4,6 +4,19 @@ class CommentsController < ApplicationController
   end
 
   def create
-    binding.pry
+    comment = Comment.new(comment_params)
+    comment.commentable_id = params[:question_id]
+    comment.commentable_type = "Question"
+    comment.user_id =  current_user.id
+    if comment.save
+      redirect_to question_path(params[:question_id])
+    else
+      render :new
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:body)
   end
 end
