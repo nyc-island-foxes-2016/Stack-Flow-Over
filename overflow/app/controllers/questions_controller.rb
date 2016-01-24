a1b05c055d0140f4a368c58887c918c9f76e0cc8
Sasha Tlr <sashatailor@gmail.com>
2016-01-23 19:00:44 -0500

    Add user names and created at times to questions, answers, comments. Add new comment and answer links to nested comments and answers

--- overflow/app/controllers/questions_controller.rb
+++ overflow/app/controllers/questions_controller.rb
@@ -3,7 +3,7 @@ class QuestionsController < ApplicationController
 before_action :set_question, except: [:index, :new, :create, :show]
 
   def index
-    @questions = Question.all
+    @questions = Question.includes(:user)
   end
 
   def new
