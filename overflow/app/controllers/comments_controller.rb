a8b9e9c98547480d6cb88d5b7920e26eefaf2b4e
Natalie Polen <email": null>
2016-01-24 13:21:21 -0500

    Uncomment Sasha's code with logic for handling comments on questions OR answers

--- overflow/app/controllers/comments_controller.rb
+++ overflow/app/controllers/comments_controller.rb
@@ -10,20 +10,20 @@ class CommentsController < ApplicationController
 
   def create
     comment = Comment.new(comment_params)
-    comment.commentable_id = params[:question_id]
-    # if !!params[:question_id]
-    #   comment.commentable_id = params[:question_id]
-    #   comment.commentable_type = "Question"
-    #   redirect_path_name = params[:question_id]
-    # else
-    #   comment.commentable_id = params[:answer_id]
-    #   comment.commentable_type = "Answer"
-    #   redirect_path_name = Answer.find(params[:answer_id]).question_id
-    # end
+    if !!params[:question_id]
+      comment.commentable_id = params[:question_id]
+      comment.commentable_type = "Question"
+      redirect_path_name = params[:question_id]
+    else
+      comment.commentable_id = params[:answer_id]
+      comment.commentable_type = "Answer"
+      redirect_path_name = Answer.find(params[:answer_id]).question_id
+    end
+
     comment.user_id =  current_user.id
+
     if comment.save
-      redirect_to question_path(params[:question_id])
-      # redirect_to question_path(redirect_path_name)
+      redirect_to question_path(redirect_path_name)
     else
       render :new
     end
