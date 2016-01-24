015bf18ed9b6497ec8d2f7c5b10211d3a21b4ca1
Chris Guard <cguard90@gmail.com>
2016-01-24 14:16:19 -0500

    checkpoint for rebase on votes controller

--- overflow/app/controllers/votes_controller.rb
+++ overflow/app/controllers/votes_controller.rb
@@ -1,7 +1,39 @@
 class VotesController < ApplicationController
   def new
+    if @vote = Vote.new(vote_params)
+      binding.pry
+    end
   end
 
   def create
+    @vote = Vote.new(vote_params)
+    binding.pry
+
   end
+
+  private
+  def vote_params
+    {vote_type: params[:vote_type], user_id: current_user.id, voteable_type: voteable_type_setter, voteable_id: voteable_id_setter }
+  end
+
+  def voteable_type_setter
+    if params[:question_id]
+      voteable_type = "Question"
+    elsif params[:comment_id]
+      voteable_type = "Comment"
+    elsif params[:answer_id]
+        voteable_type = "Answer"
+    end
+  end
+
+  def voteable_id_setter
+    if params[:question_id]
+      voteable_id = params[:question_id]
+    elsif params[:comment_id]
+      voteable_id = params[:comment_id]
+    elsif params[:answer_id]
+      voteable_id = params[:answer_id]
+    end
+  end
+
 end
