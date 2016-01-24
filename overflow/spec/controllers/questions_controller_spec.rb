5d8bbfd259fabc435381d589fcf694391ab0ae26
Sasha <email": null>
2016-01-24 14:24:20 -0500

    Add tests for index in controller spec file

--- overflow/spec/controllers/questions_controller_spec.rb
+++ overflow/spec/controllers/questions_controller_spec.rb
@@ -4,10 +4,24 @@ describe QuestionsController do
   let(:question) {FactoryGirl.create :question}
 
   describe "#index" do
-    it "assignes the questions to Question.all" do
+    it "assigns the questions to Question.all" do
       get :index
       expect(response).to be_success
     end
+
+    it "populates an array of questions" do
+      question = FactoryGirl.create(:question)
+      get :index
+      assigns(:questions).should eq([question])
+    end
+
+    it "renders the :index view" do
+      get :index
+      response.should render_template :index
+    end
+
   end
+
+
 
 end
